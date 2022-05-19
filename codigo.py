761d0902b2ffaa28ab50b6d8852a91f8c8b1d3d1
viniciusgarlopes <viniciusgl2@al.insper.edu.br>
2022-05-19 15:30:52 -0300

    Coloquei imagens das bandeiras e tela secundaria

--- codigo.py
+++ codigo.py
@@ -12,6 +12,16 @@ pygame.display.set_caption('SAVE IF YOU CAN') #definido o titulo da janela
 game = True 
 imagem_inicial = pygame.image.load('assets/allianz-parque.jpg').convert() #imagem inicial
 imagem_inicial = pygame.transform.scale(imagem_inicial, (largura,altura)) #definir tamanho da imagem
+brasil = pygame.image.load('assets/brasil.png').convert()
+brasil = pygame.transform.scale(brasil, (300,150))
+argentina = pygame.image.load('assets/argentina.png').convert()
+argentina = pygame.transform.scale(argentina, (300,150))
+alemanha = pygame.image.load('assets/alemanha.png').convert()
+alemanha = pygame.transform.scale(alemanha, (300,150))
+franca = pygame.image.load('assets/franca.png').convert()
+franca = pygame.transform.scale(franca, (300,150))
+gramado = pygame.image.load('assets/gramado.png').convert()
+gramado = pygame.transform.scale(gramado, (largura, altura))
 
 while game: #looping enquanto game = True
     for event in pygame.event.get(): 
@@ -19,6 +29,17 @@ while game: #looping enquanto game = True
             game = False
 
     window.blit(imagem_inicial, (0, 0))
+
+    if event.type == pygame.KEYUP:
+        window.blit(gramado, (0, 0))
+        window.blit(brasil, ((largura/2+50), (altura/2-200)))
+        window.blit(argentina, ((largura/2+50), (altura/2+50)))
+        window.blit(alemanha, ((largura/2-350), (altura/2-200)))
+        window.blit(franca, ((largura/2-350), (altura/2+50)))
+    
     pygame.display.update()
+
+    
+
 
 pygame.quit()
