20086496b6fb1a7d7b58265aa5889888f51014d8
viniciusgarlopes <viniciusgl2@al.insper.edu.br>
2022-05-20 19:19:13 -0300

    Botao de jogar , cores, funcao de texto

--- codigo.py
+++ codigo.py
@@ -1,4 +1,6 @@
 #importar bibliotecas
+from cgitb import text
+from tkinter import font
 import pygame 
 
 pygame.init() #iniciando o pygame
@@ -6,6 +8,7 @@ pygame.init() #iniciando o pygame
 altura = 700
 largura = 1500
 window = pygame.display.set_mode((largura, altura)) #tamanho da janela
+font = pygame.font.SysFont(None, 100)
 
 pygame.display.set_caption('SAVE IF YOU CAN') #definido o titulo da janela
 
@@ -22,21 +25,40 @@ franca = pygame.image.load('assets/franca.png').convert()
 franca = pygame.transform.scale(franca, (300,150))
 gramado = pygame.image.load('assets/gramado.png').convert()
 gramado = pygame.transform.scale(gramado, (largura, altura))
+branco = (255,255,255)
+preto = (0,0,0)
+vermelho=(255,0,0)
+verde = (0,255,0)
+azul = (0,0,255)
+
+
+def texto (msg, cor):
+    texto1 = font.render(msg, True, cor)
+    window.blit(texto1, [650, 280])
+    pygame.display.flip
 
 while game: #looping enquanto game = True
     for event in pygame.event.get(): 
         if event.type == pygame.QUIT:
             game = False
-
-    window.blit(imagem_inicial, (0, 0))
-
-    if event.type == pygame.KEYUP:
-        window.blit(gramado, (0, 0))
-        window.blit(brasil, ((largura/2+50), (altura/2-200)))
-        window.blit(argentina, ((largura/2+50), (altura/2+50)))
-        window.blit(alemanha, ((largura/2-350), (altura/2-200)))
-        window.blit(franca, ((largura/2-350), (altura/2+50)))
     
+    window.blit(imagem_inicial, (0, 0))
+    pygame.draw.rect(window, verde, [500, 250, 500, 150])
+    texto('Jogar', branco)
+
+
+
+
+    if event.type == pygame.MOUSEBUTTONDOWN:
+        x = pygame.mouse.get_pos()[0]
+        y =  pygame.mouse.get_pos()[1]
+        if x > 500 and y > 250 and x < 1000 and y < 650:
+            window.blit(gramado, (0, 0))
+            window.blit(brasil, ((largura/2+50), (altura/2-200)))
+            window.blit(argentina, ((largura/2+50), (altura/2+50)))
+            window.blit(alemanha, ((largura/2-350), (altura/2-200)))
+            window.blit(franca, ((largura/2-350), (altura/2+50)))
+            
     pygame.display.update()
 
     
